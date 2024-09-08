9b4bb6f494a503568db8cd777a48f5d6ee69e58b
KamleshPatel98 <kamleshp52170@gmail.com>
2024-09-08 17:17:33 +0530

    patter

---
+++ pattern.php
@@ -0,0 +1,132 @@
+<?php
+    /* square paatern
+        1 2 3 4
+        1 2 3 4
+        1 2 3 4
+        1 2 3 4
+    */
+    $n=4;
+    // for($i=1; $i<=$n; $i++){ //number of columns
+    //     for($j=1; $j<=$n; $j++){
+    //         echo " ".$j; //1 2 3 4
+    //     }
+    //     echo "<br>";
+    // }
+
+    /*
+        * * * *
+        * * * *
+        * * * *
+        * * * *
+    */
+    // for($i=0; $i<$n; $i++){
+    //     for($j=0; $j<$n; $j++){
+    //         echo "* ";
+    //     }
+    //     echo "<br>";
+    // }
+
+    /*
+        A B C D
+        A B C D
+        A B C D
+        A B C D
+    */
+    // for($i=0; $i<$n; $i++){
+    //     $char="A"; //for reset
+    //     for($j=0; $j<$n; $j++){
+    //         echo $char++." ";
+    //     }
+    //     echo "<br>";
+    // }
+
+    /*
+        1 2 3 4
+        5 6 7 8
+        9 10 11 12
+        13 14 15 16
+    */
+    // $num=1; //for no reset
+    // for($i=0; $i<$n; $i++){
+    //     for($j=0; $j<$n; $j++){
+    //         echo $num++ ." ";
+    //     }
+    //     echo "<br>";
+    // }
+
+    /*
+        A B C
+        D E F
+        G H I
+    */
+    // $n=3;
+    // $char="A";
+    // for($i=0; $i<$n; $i++){
+    //     for($j=0; $j<$n; $j++){
+    //         echo $char++ ." ";
+    //     }
+    //     echo "<br>";
+    // }
+
+    //traingle pattern
+    /*
+        *
+        * *
+        * * *
+        * * * *
+    */
+    // for($i=0; $i<$n; $i++){
+    //     for($j=0; $j<$i+1; $j++){
+    //         echo "* ";
+    //     }
+    //     echo "<br>";
+    // }
+
+    /*
+        1
+        2 2
+        3 3 3
+        4 4 4 4
+    */
+    // for($i=0; $i<$n; $i++){
+    //     for($j=0; $j<$i+1; $j++){
+    //         echo $i + 1;
+    //         echo " ";
+    //     }
+    //     echo "<br>";
+    // }
+
+    /*
+        A
+        BB
+        CCC
+        DDDD
+    */
+    // $char="A";
+    // for($i=0; $i<$n; $i++){
+    //     for($j=0; $j<$i+1; $j++){
+    //         echo $char;
+    //     }
+    //     $char++;
+    //     echo "<br>";
+    // }
+
+    // $n=5;
+    // for($i=0; $i<$n; $i++){
+    //     for($j=1; $j<=$i; $j++){
+    //         echo $j ;
+    //         echo " ";
+    //     }
+    //     $j++;
+    //     echo "<br>";
+    // }
+
+    //reverse traingle
+    // for($i=0; $i<$n; $i++){
+    //     for($j=$i+1; $j>0; $j--){
+    //         echo $j;
+    //     }
+    //     echo "<br>";
+    // }
+
+?>
